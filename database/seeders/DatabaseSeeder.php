3ea7d90ccd78c1b16b3e244db450e85aa63466a8
Mahmoud Ghanem <moghanem345@gmail.com>
2023-06-04 01:57:45 +0300

    Initial Seeding for the Database Tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,48 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $work = Category::create([
+                'name' => 'Work',
+                'slug' => 'work'
+	    ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        Post::create([
+           'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-first-post',
+            'excerpt' => ' fjdkfjd kfjd ',
+            'body' => 'dkfjdkf dfk djkfj kj fkdjf dkj dkfjd kfjd '
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-second-post',
+            'excerpt' => ' fjdkfjd kfjd ',
+            'body' => 'dkfjdkf dfk djkfj kj fkdjf dkj dkfjd kfjd '
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-third-post',
+            'excerpt' => ' fjdkfjd kfjd ',
+            'body' => 'dkfjdkf dfk djkfj kj fkdjf dkj dkfjd kfjd '
+        ]);
     }
 }
