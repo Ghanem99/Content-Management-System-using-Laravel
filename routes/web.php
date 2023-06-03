be1a9ef98dfcbfce205fbd618989ef79e1a4cb8d
Mahmoud Ghanem <moghanem345@gmail.com>
2023-06-03 15:14:03 +0300

    Explain the Route Model Binding Cnocept

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('posts/{post}', function ($id) {
-    return view('post', [
-        'post' => Post::find($id)
-    ]);
+Route::get('posts/{post}', function (Post $post) {
+   return view('post', [
+       'post' => $post
+   ]);
 });
