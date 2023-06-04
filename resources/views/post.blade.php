b7623f2b3a0d275c133d618b9f88417140a212b6
Mahmoud Ghanem <moghanem345@gmail.com>
2023-06-04 10:34:05 +0300

    Change ->user->name to ->author->name

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,6 +2,12 @@
 {{--    <h1> {{ $post->title }}</h1>--}}
 {{--    use this when you have a controll over this data--}}
     <h1> {!! $post->title !!}</h1>
+    <div>
+{{--        from user to author: your code now, reflects the way you speak.--}}
+        written by <a href="">{{ $post->author->name }}</a>
+        in <a href="">{{ $post->category->name }}</a>
+    </div>
+    <br>
 
 
     <div>
